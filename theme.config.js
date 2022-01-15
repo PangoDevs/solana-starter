b8cb9e2a48adae8ee36aa7d352035db30750f185
Vitor <vi.souza.almeida@protonmail.com>
2022-01-15 18:44:28 -0300

    feat: New og:image on theme config

--- theme.config.js
+++ theme.config.js
@@ -16,14 +16,26 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta
+        name="description"
+        content="Pango: project to learn more about Solana"
+      />
+      <meta
+        name="og:description"
+        content="Pango: project to learn more about Solana"
+      />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
+      <meta
+        name="twitter:image"
+        content="https://raw.githubusercontent.com/PangoDevs/solana-starter/main/public/og.png?token=GHSAT0AAAAAABQC57VQ2L5KSH5PYD3DDCL6YPDIBAA"
+      />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
       <meta name="twitter:url" content="https://nextra.vercel.app" />
       <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+      <meta
+        name="og:image"
+        content="https://raw.githubusercontent.com/PangoDevs/solana-starter/main/public/og.png?token=GHSAT0AAAAAABQC57VQ2L5KSH5PYD3DDCL6YPDIBAA"
+      />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link
         rel="apple-touch-icon"
